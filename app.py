1b38dc4db355ac5e1088ff2afdc9431433dd18a8
dolsen0 <derek.olsen@gmail.com>
2023-02-15 01:07:20 -0500

    updated to include fight stats

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from apiKey import apiKey
 
 conn = http.client.HTTPSConnection("api.sportradar.us")
 
-conn.request("GET", "/mma/trial/v2/en/champions.xml?api_key=" + apiKey)
+#conn.request("GET", "/mma/trial/v2/en/champions.json?api_key=" + apiKey)
+#conn.request("GET", "/mma/trial/v2/en/schedules/2023-02-11/summaries.json?api_key=" + apiKey)
 
 res = conn.getresponse()
 data = res.read()
