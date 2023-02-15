7dd1c4ff0bdd161e1317a50d2b2b57c7b440277f
dolsen0 <derek.olsen@gmail.com>
2023-02-15 14:44:30 -0500

    added ability to pull up information on individual fighters

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from apiKey import apiKey
 conn = http.client.HTTPSConnection("api.sportradar.us")
 
 #conn.request("GET", "/mma/trial/v2/en/champions.json?api_key=" + apiKey)
+
 #conn.request("GET", "/mma/trial/v2/en/schedules/2023-02-11/summaries.json?api_key=" + apiKey)
+
+#conn.request("GET", "/mma/trial/v2/en/competitions.json?api_key=" + apiKey)
+
+#pulls info on Aljo
+#conn.request("GET", "/mma/trial/v2/en/competitors/sr:competitor:260623/profile.json?api_key=" + apiKey)
+
+#pulls info on Valentina
+conn.request("GET", "/mma/trial/v2/en/competitors/sr:competitor:246053/profile.json?api_key=" + apiKey)
+
 
 res = conn.getresponse()
 data = res.read()
